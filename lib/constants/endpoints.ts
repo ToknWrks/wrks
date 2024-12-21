c964c24ac9db75176715a0eba1b44da029781896
ToknWrks <133374682+ToknWrks@users.noreply.github.com>
2024-12-21 07:45:45 -0500

    Update lib/constants/endpoints.ts [BoltSync]

---
+++ lib/constants/endpoints.ts
@@ -0,0 +1,38 @@
+export const FALLBACK_ENDPOINTS = {
+  'osmosis': {
+    rest: 'https://rest.cosmos.directory/osmosis',
+    rpc: 'https://rpc.cosmos.directory/osmosis'
+  },
+  'cosmoshub': {
+    rest: 'https://rest.cosmos.directory/cosmoshub',
+    rpc: 'https://rpc.cosmos.directory/cosmoshub'
+  },
+  'akash': {
+    rest: 'https://rest.cosmos.directory/akash',
+    rpc: 'https://rpc.cosmos.directory/akash'
+  },
+  'celestia': {
+    rest: 'https://rest.cosmos.directory/celestia',
+    rpc: 'https://rpc.cosmos.directory/celestia'
+  },
+  'regen': {
+    rest: 'https://rest.cosmos.directory/regen',
+    rpc: 'https://rpc.cosmos.directory/regen'
+  },
+  'juno': {
+    rest: 'https://rest.cosmos.directory/juno',
+    rpc: 'https://rpc.cosmos.directory/juno'
+  },
+  'dydx': {
+    rest: 'https://rest.cosmos.directory/dydx',
+    rpc: 'https://rpc.cosmos.directory/dydx'
+  },
+  'saga': {
+    rest: 'https://rest.cosmos.directory/saga',
+    rpc: 'https://rpc.cosmos.directory/saga'
+  },
+  'omniflixhub': {
+    rest: 'https://rest.cosmos.directory/omniflixhub',
+    rpc: 'https://rpc.cosmos.directory/omniflixhub'
+  }
+} as const;
